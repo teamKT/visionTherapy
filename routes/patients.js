7fec43c6f2923b9cef31f69cf04f4d679509c720
Gunther Schneider <pixeloon@gmail.com>
2016-04-25 15:21:37 -0700

    added patient routes and add patient on doc dash

--- routes/patients.js
+++ routes/patients.js
@@ -1,9 +1,10 @@
 const express = require("express");
-const router = express.Router()
+const router = express.Router({ mergeParams: true })
 const knex = require("../db/knex");
 const helpers = require('../helpers/authHelpers');
 
 // PatientInfo
+
 router.get('/', helpers.currentUser, (req,res) => {
   if(req.isAuthenticated()){
     res.redirect(`/doctors/${req.params.doctor_id}/patients/${req.user.id}`)
@@ -34,4 +35,44 @@ router.get('/:id', helpers.currentUser, (req,res) => {
     })
 });
 
+
+//EDIT
+router.get('/edit/:id', helpers.currentUser, (req, res) => {
+    knex('patients').where("id", +req.params.id).first().then(patient => {
+        res.render("patients/edit", { patient })
+    });
+});
+
+//NEW
+router.get('/new', helpers.currentUser, (req, res) => {
+    res.render("patients/new")
+});
+
+// POST
+router.post('/', helpers.currentUser, (req, res) => {
+    knex('patients')
+        .insert({
+            childname: req.body.patient.childname,
+            username: req.body.patient.username,
+            parentname: req.body.patient.parentname,
+            doctor_id: +req.params.doctor_id
+        })
+        .then(() => {
+            res.redirect(`/doctors/${req.params.doctor_id}`)
+        }).catch(err => {
+            console.log("Error Message: ", err)
+            res.redirect(`/doctors/${req.params.doctor_id}`)
+        })
+});
+
+
+// PUT
+router.put('/:id', helpers.currentUser, (req, res) => {
+    knex('patients').update(req.body.patient).where('id', +req.params.id)
+        .then(() => {
+            res.redirect(`/doctors/${req.params.doctor_id}`)
+        });
+});
+
 module.exports = router;
+>>>>>>> added patient routes and add patient on doc dash
