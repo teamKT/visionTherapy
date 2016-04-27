61435e20b6c9ddd9d2aa72be6fe23af90bff50b8
Alexander Chang <gxchang@gmail.com>
2016-04-27 14:02:36 -0700

    added production knexfile

--- knexfile.js
+++ knexfile.js
@@ -13,7 +13,21 @@ module.exports = {
     },
     debug: false
   },
-
+  production: {
+      client: 'pg',
+      // connection: {
+      //   database: 'my_db',
+      //   user:     'username',
+      //   password: 'password'
+      // },
+      pool: {
+        min: 2,
+        max: 10
+      },
+      migrations: {
+        tableName: 'knex_migrations'
+      }
+  },
   test: {
     client: 'pg',
     connection: {
