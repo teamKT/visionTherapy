e5dd1c5556dbb16f54b58edebb02ab49f8920b85
Alexander Chang <gxchang@gmail.com>
2016-04-27 13:26:02 -0700

    added error 404 route

--- app.js
+++ app.js
@@ -35,6 +35,23 @@ app.use('/auth', routes.auth);
 app.use('/doctors', routes.doctors);
 app.use('/', routes.statics);
 
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
 // listener
 app.listen(3000, function() {
   console.log('starting localhost:3000');
