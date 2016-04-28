a8b27918fd362f90ca3ec0899f264c67cbaca15d
Alexander Chang <gxchang@gmail.com>
2016-04-28 15:51:17 -0700

    removed all seeds except exercises

--- seeds/doctors.js
+++ seeds/doctors.js
@@ -1,21 +1,21 @@
 exports.seed = function(knex, Promise) {
   return Promise.all([
-    knex('doctors').del(),
-    knex('doctors').insert({
-      isDoctor: true,
-      firstname: 'Joe', 
-      lastname: 'Smith', 
-      email: 'joe@gmail.com', 
-      username: 'joe',
-      password: '$2a$10$jW.51sVsOQEbtuO1nM6.juxCnO9DiZsLg8jghQdIT6AlKvifKa.qa' 
-    }),
-    knex('doctors').insert({
-      isDoctor: true,
-      firstname: 'Mary', 
-      lastname: 'Smith', 
-      email: 'mary@gmail.com', 
-      username: 'mary',
-      password: '$2a$10$jW.51sVsOQEbtuO1nM6.juxCnO9DiZsLg8jghQdIT6AlKvifKa.qa'
-    })
+    // knex('doctors').del(),
+    // knex('doctors').insert({
+    //   isDoctor: true,
+    //   firstname: 'Joe', 
+    //   lastname: 'Smith', 
+    //   email: 'joe@gmail.com', 
+    //   username: 'joe',
+    //   password: '$2a$10$jW.51sVsOQEbtuO1nM6.juxCnO9DiZsLg8jghQdIT6AlKvifKa.qa' 
+    // }),
+    // knex('doctors').insert({
+    //   isDoctor: true,
+    //   firstname: 'Mary', 
+    //   lastname: 'Smith', 
+    //   email: 'mary@gmail.com', 
+    //   username: 'mary',
+    //   password: '$2a$10$jW.51sVsOQEbtuO1nM6.juxCnO9DiZsLg8jghQdIT6AlKvifKa.qa'
+    // })
   ]);
 };
